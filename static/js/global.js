dddcfff392609a35122ae4e63e0414fcc288ebeb
Privato Produttore <privatemaker@posteo.net>
2019-06-18 16:12:04 +0000

    adjust to global.js

--- static/js/global.js
+++ static/js/global.js
@@ -1,23 +1,26 @@
-const dev = false
+const dev = true
 
 let url_feed = "https://testing.tracking.exposed/feeds/0"
 if (dev) {
     url_feed = "https://testing.tracking.exposed/feeds/0"
 }
 
-function getToken() {
+var getToken = function() {
   if (dev) {
-    let t = "testing-token"
+    let token = "testing-token"
+    return token;
   } else {
-    let t = _.find(window.location.pathname.split('/'), function(e) {
+    let token = _.find(window.location.pathname.split('/'), function(e) {
         return _.size(e) == 40;
     });
+    if (!token) {
+      console.log("Wrong token length in the URL");
+      return "";
+    }
   }
-  if(!t) console.log("Wrong token length in the URL");
-  return t;
 }
 
-buildApiUrl = function(end) {
+var buildApiUrl = function(end) {
 
     let api_path = "/api/v2"
     if (dev) {
