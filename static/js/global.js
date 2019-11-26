4717a54a216457e49c85fa163b39853164ae8987
Claudio Agosti <claudio@tracking.exposed>
2019-11-26 13:09:41 +0100

    fixed global

--- static/js/global.js
+++ static/js/global.js
@@ -1,11 +1,5 @@
 
-let url_feed = "https://testing.tracking.exposed/feeds/0"
-
 var getToken = function() {
-  /*
-    let token = "testing-token"
-    return token;
-  */
   const token = window.location.href.split('/').pop().substr(1, 40);
   if (_.size(token) != 40 ) {
     console.log("Wrong token length in the URL");
@@ -16,8 +10,8 @@ var getToken = function() {
 
 var buildApiUrl = function(end) {
   let api_path = "/api/v2"
-  const host = 'https://testing.tracking.exposed' // window.location.origin;
+  const host = window.location.origin;
   const ret = `${host}${api_path}${end}`
-  console.log(`double check: URL composed ${ret}`);
+  console.log(`buildApiUrl composed ${ret}`);
   return ret;
 }
