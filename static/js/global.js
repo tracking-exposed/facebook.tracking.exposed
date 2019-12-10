33d0129a742b962347468e95af5574b9b0e34600
Claudio <claudio@tracking.exposed>
2019-12-10 17:44:54 +0100

    fixed static base url of zuckless news

--- static/js/global.js
+++ static/js/global.js
@@ -1,5 +1,5 @@
 
-const url_feed = '/feeds/0'
+const url_feed = 'https://facebook.tracking.exposed/feeds/0'
 
 var getToken = function() {
   const token = window.location.href.split('/').pop().substr(1, 40);
